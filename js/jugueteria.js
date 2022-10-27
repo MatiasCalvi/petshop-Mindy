c3eae8adc50ab6c45866c3af6624be7f3109bcc6
MatiasCalvi <alejandropreuss30@gmail.com>
2022-10-27 02:03:27 -0300

    filtro cruzado

--- js/jugueteria.js
+++ js/jugueteria.js
@@ -1,9 +1,12 @@
-let contenedorCards=document.getElementById("contenedor-cards")
-let contenedorSelect=document.getElementById("contenedor-select")
-let searchBar=document.getElementById("searchBar")
+let contenedorCards = document.getElementById("contenedor-cards");
+let contenedorSelect = document.getElementById("contenedor-select");
+let searchBar = document.getElementById("searchBar");
+let select = document.getElementById("pet-items-select");
+let aplicado = {};
+let selectFiltrado=[]
 
-function htmlCard(array){
-    contenedorCards.innerHTML+=`
+function htmlCarta(array) {
+  contenedorCards.innerHTML += `
      <div class="card" style="width: 16rem" data-aos="fade-up">
         <img
           src="${array.imagen}"
@@ -30,11 +33,11 @@ function htmlCard(array){
           <a href=""><button class="movile-button">More info</button></a>
         </div>
       </div> 
-      `
+      `;
 }
 
-function htmlCardPocoStock(array){
-    contenedorCards.innerHTML+=`
+function htmlCartaPocoStock(array) {
+  contenedorCards.innerHTML += `
     <div class="card" style="width: 16rem" data-aos="fade-up">
        <img
          src="${array.imagen}"
@@ -61,53 +64,95 @@ function htmlCardPocoStock(array){
          <a href=""><button class="movile-button">More info</button></a>
        </div>
      </div> 
-     `
+     `;
 }
 
-function printHtml(arraySuperior,arrayInferior){
-    arraySuperior.forEach((e)=> htmlCard(e));
-    arrayInferior.forEach((e)=> htmlCardPocoStock(e))
+function imprimir(arraySuperior, arrayInferior) {
+  arraySuperior.forEach((e) => htmlCarta(e));
+  arrayInferior.forEach((e) => htmlCartaPocoStock(e));
 }
 
-function condicionPocoStock(array){
-    array=array.filter(e=>e.stock<=3)
-    return array
+function condicionPocoStock(array) {
+  array = array.filter((e) => e.stock <= 3);
+  return array;
 }
 
-function condicionStockMayor(array){
-    array=array.filter(e=>e.stock>3)
-    return array
+function condicionStockMayor(array) {
+  array = array.filter((e) => e.stock > 3);
+  return array;
 }
 
-function searchBarfn(evento,array){
-    contenedorCards.innerHTML=""
-    array = array.filter((event) =>
-        event.name.toLowerCase().includes(evento.target.value.toLowerCase())
-    );
+function searchBarfn(evento, array) {
+  contenedorCards.innerHTML = "";
+  array = array.filter((event) =>
+    event.name.toLowerCase().includes(evento.target.value.toLowerCase())
+  );
 }
 
-async function capturar(){
-    try{
-        let api= await fetch("https://apipetshop.herokuapp.com/api/articulos");
-        var data= await api.json();
-        data=data.response;
-        let juguetes=data.filter(e=>e.tipo==="Juguete")
-        printHtml(condicionStockMayor(juguetes),condicionPocoStock(juguetes))
-        searchBar.addEventListener("keyup",(evento)=>{
-            let busqueda=juguetes.filter(card=>card.nombre.toLowerCase().includes(evento.target.value))
-            contenedorCards.innerHTML=""
-            busqueda.forEach(e=>htmlCard(e))
-            if(busqueda.length === 0){
-                contenedorCards.innerHTML =` 
+function filtrarFn(especialidad, valor, array) {
+  aplicado[especialidad] = valor;
+
+  for (let dato in aplicado) {
+    if (dato === "datoPorSearchBar") {
+       array = array.filter((card) =>
+        card.nombre.toLowerCase().includes(aplicado[dato].toLowerCase())
+      );
+    }
+
+    if (dato === "datoPorSelect") {
+        if (valor == 1) {
+           array.filter((card) => card.precio > 500);
+        }
+          if (valor != 1) {
+           array.filter((card) => card.precio <= 500);
+        }
+    }
+  }
+  console.log(aplicado)
+  return array
+}
+
+async function capturar() {
+  try {
+    let api = await fetch("https://apipetshop.herokuapp.com/api/articulos");
+    var data = await api.json();
+    data = data.response;
+    let juguetes = data.filter((e) => e.tipo === "Juguete");
+    imprimir(condicionStockMayor(juguetes), condicionPocoStock(juguetes));
+    searchBar.addEventListener("keyup", (evento) => {
+     /*  let busqueda = juguetes.filter((card) =>
+        card.nombre.toLowerCase().includes(evento.target.value)
+      );
+      contenedorCards.innerHTML = "";
+      busqueda.forEach((e) => htmlCarta(e));
+      if (busqueda.length === 0) {
+        contenedorCards.innerHTML = ` 
                 <div style="min-height:50vh;">
                    <img class="error"  height="400"width="350" src="../assets/img/404.png" alt="page not found">
                 </div>
-                `
-            }
-        })   
-    }
-    catch(error){
-        console.log(error)
-    }
+                `;
+      } */
+      let escribir=filtrarFn("datoPorSearchBar", evento.target.value, juguetes)
+      escribir.forEach(e=>htmlCarta(e))
+      console.log(escribir)
+    });
+    select.addEventListener("change", (evento) => {
+      /* let mayor = juguetes.filter((card) => card.precio > 500);
+      let menor = juguetes.filter((card) => card.precio <= 500);
+      contenedorCards.innerHTML = "";
+      console.log(evento.target.value);
+      if (evento.target.value == 1) {
+        menor.forEach((e) => htmlCarta(e));
+      }
+      if (evento.target.value != 1) {
+        mayor.forEach((e) => htmlCarta(e));
+      } */
+      let seleccionar=filtrarFn("datoPorSelect", evento.target.value, juguetes)
+      seleccionar.forEach(e=>htmlCarta(e))
+      console.log(seleccionar)
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-capturar() 
+capturar();
