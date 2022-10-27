f3e59a6534f558d9ce8192c6e9b2c992d3c35c15
ignaarguello <igna.arguello2110@hotmail.com>
2022-10-27 13:57:01 -0300

    Correciones al JS de farmacia y a los estilos.css

--- js/farmacia.js
+++ js/farmacia.js
@@ -7,34 +7,16 @@ let aplicado = {};
 function htmlCarta(array) {
   contenedorCards.innerHTML += `
      <div class="card" style="width: 16rem" data-aos="fade-up">
-        <img
-          src="${array.imagen}"
-          class="card-img-top images"
-          alt="${array.nombre}"
-        />
+        <img src="${array.imagen}" class="card-img-top images" alt="${array.nombre}"/>
         <div class="category-div">
           <span class="tag tag-teal">${array.tipo}</span>
         </div>
         <div class="card-body">
           <h5 class="titlee">${array.nombre}</h5>
           <p>Price: ${array.precio}</p>
-          <p class="card-text">
-            ${array.descripcion}
-          </p>
-          <button class="btn-comprar card-button">
-            <img
-              class="btn-comprar-img"
-              src="../assets/img/carritoBtn.png"
-              height="30px"
-              alt="huellita"
-            />
-          </button>
-          <a href=""><button class="movile-button"> <img
-          class="btn-comprar-img"
-          src="../assets/img/carritoBtn.png"
-          height="30px"
-          alt="huellita"
-        /></button></a>
+          <p class="card-text"> ${array.descripcion}</p>
+          <button class="btn-comprar card-button"><img class="btn-comprar-img" src="../assets/img/carritoBtn.png" height="30px" alt="huellita"/></button>
+          <button class="btn-comprar movile-button"> <imgclass="btn-comprar-img" src="../assets/img/carritoBtn.png" height="30px" alt="huellita"/></button>
         </div>
       </div> 
       `;
@@ -43,34 +25,19 @@ function htmlCarta(array) {
 function htmlCartaPocoStock(array) {
   contenedorCards.innerHTML += `
     <div class="card" style="width: 16rem" data-aos="fade-up">
-       <img
-         src="${array.imagen}"
-         class="card-img-top images"
-         alt="${array.nombre}"
-       />
+       <img src="${array.imagen}" class="card-img-top images" alt="${array.nombre}"/>
        <div class="category-div">
          <span class="tag tag-teal">${array.tipo}</span>
        </div>
        <div class="card-body">
          <h5 class="titlee">${array.nombre}</h5>
          <p>Price: ${array.precio}</p>
-         <p class="card-text">
-           POCO STOCK
+         <p class="card-description"> ${array.descripcion}</p>
+         <p class="card-text" style='color:red; font-size:1.1rem;'>
+           ! Ultimas Unidades !
          </p>
-         <button class="btn-comprar card-button">
-           <img
-             class="btn-comprar-img"
-             src="../assets/img/carritoBtn.png"
-             height="30px"
-             alt="huellita"
-           />
-         </button>
-         <a href=""><button class="movile-button"><img
-         class="btn-comprar-img"
-         src="../assets/img/carritoBtn.png"
-         height="30px"
-         alt="huellita"
-       /></button></a>
+         <button class="btn-comprar card-button"><img class="btn-comprar-img" src="../assets/img/carritoBtn.png" height="30px" alt="huellita"/></button>
+         <button class="btn-comprar movile-button"><img class="btn-comprar-img" src="../assets/img/carritoBtn.png" height="30px" alt="huellita"/></button>
        </div>
      </div> 
      `;
@@ -99,7 +66,7 @@ function searchBarfn(evento, array) {
 }
 
 function filtrarFn(especialidad, valor, array) {
-  
+
   aplicado[especialidad] = valor;
 
   contenedorCards.innerHTML = "";
@@ -114,20 +81,19 @@ function filtrarFn(especialidad, valor, array) {
     if (dato === "datoPorSelect") {
       if (aplicado["datoPorSelect"] == 1) {
         array = array.filter((card) => card.precio <= 500);
-      } 
-      if(aplicado["datoPorSelect"] == 2){
+      }
+      if (aplicado["datoPorSelect"] == 2) {
         array = array.filter((card) => card.precio > 500);
       }
-      if(aplicado["datoPorSelect"] == 3){
-         return array
+      if (aplicado["datoPorSelect"] == 3) {
+        return array
       }
     }
-    if(array.length=== 0){
-      contenedorCards.innerHTML= contenedorCards.innerHTML =` 
+    if (array.length === 0) {
+      contenedorCards.innerHTML = contenedorCards.innerHTML = ` 
       <div style="min-height:50vh;">
          <img class="error"  height="400"width="350" src="../assets/img/404.png" alt="page not found">
-      </div>
-      `
+      </div>`
     }
   }
   return array;
@@ -138,17 +104,23 @@ async function capturar() {
     let api = await fetch("https://apipetshop.herokuapp.com/api/articulos");
     var data = await api.json();
     data = data.response;
-    let juguetes = data.filter((e) => e.tipo === "Juguete");
-    imprimir(condicionStockMayor(juguetes), condicionPocoStock(juguetes));
+    console.log('data', data)
+    
+    let medicamentos = data.filter((e) => e.tipo === "Medicamento");
+    imprimir(condicionStockMayor(medicamentos), condicionPocoStock(medicamentos));
     searchBar.addEventListener("keyup", (evento) => {
-      let escribir = filtrarFn("datoPorSearchBar",evento.target.value,juguetes);
-      escribir.forEach(e=>(e.stock<=3)?htmlCartaPocoStock(e):htmlCarta(e))
-      console.log(escribir)
+      let escribir = filtrarFn("datoPorSearchBar", evento.target.value, medicamentos);
+      escribir.forEach(e => (e.stock <= 3) ? htmlCartaPocoStock(e) : htmlCarta(e))
     });
+
+
     select.addEventListener("change", (evento) => {
-      let seleccionar = filtrarFn("datoPorSelect",evento.target.value,juguetes);
-      seleccionar.forEach(e=>(e.stock<=3)?htmlCartaPocoStock(e):htmlCarta(e))
+      let seleccionar = filtrarFn("datoPorSelect", evento.target.value, medicamentos);
+      seleccionar.forEach(e => (e.stock <= 3) ? htmlCartaPocoStock(e) : htmlCarta(e))
     });
+
+
+
   } catch (error) {
     console.log(error);
   }
